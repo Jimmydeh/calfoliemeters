5ae967425b4cd8afc424908529d2b8748fa751ab
Jimmydeh <37957155+Jimmydeh@users.noreply.github.com>
2018-03-31 15:52:01 +0200

    Create logic.py

---
+++ logic.py
@@ -0,0 +1,5 @@
+number_c = input('aantal cm folie: ')
+number_b = input('diameter bok: ')
+number_a = input('dikte folie is mm: ')
+
+print ((((number_c*2+number_b)*(number_c*2+number_b)*0.785)-(number_b*number_b)*0.785)/(number_a*10))
